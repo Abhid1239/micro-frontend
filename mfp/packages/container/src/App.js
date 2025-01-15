7d6ffed840f02b4f991dcdf42698185f8ecdbaa0
Ready to Die <54882064+Abhid1239@users.noreply.github.com>
2025-01-15 11:25:04 +0530

    feat: css handling

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core";
+
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "co"
+})
 
 export default () => {
     return (
         <BrowserRouter>
-            <div >
-                <Header />
-                <MarketingApp />
-                {/* Hello */}
-            </div>
+            <StylesProvider>
+                <div >
+                    <Header />
+                    <MarketingApp />
+                    {/* Hello */}
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
